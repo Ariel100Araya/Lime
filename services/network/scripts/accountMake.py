6ea4da9c8beffe4bf72420f94964de788759828c
Ariel Araya-Madrigal <Ariel100araya@outlook.com>
2021-09-13 18:49:52 -0400

    update

--- services/network/scripts/accountMake.py
+++ services/network/scripts/accountMake.py
@@ -1,23 +1,23 @@
 import shutil
 import os
 
-newpath = r'/Users/ariel/Documents/Developer/Web Projects/Lime/services/network/per/usr-2' 
+newpath = r'/Users/ariel/Documents/Developer/Web Projects/Lime/services/network/per/usr-3' 
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
 
 original = r'/Users/ariel/Documents/Developer/Web Projects/Lime/services/network/scripts/samplefiles/Simple.css'
-target = r'/Users/ariel/Documents/Developer/Web Projects/Lime/services/network/per/usr-2/Simple.css'
+target = r'/Users/ariel/Documents/Developer/Web Projects/Lime/services/network/per/usr-3/Simple.css'
 
 shutil.copyfile(original, target)
 
 original = r'/Users/ariel/Documents/Developer/Web Projects/Lime/services/network/scripts/samplefiles/accounts.html'
-target = r'/Users/ariel/Documents/Developer/Web Projects/Lime/services/network/per/usr-2/accounts.html'
+target = r'/Users/ariel/Documents/Developer/Web Projects/Lime/services/network/per/usr-3/accounts.html'
 
 shutil.copyfile(original, target)
 
 original = r'/Users/ariel/Documents/Developer/Web Projects/Lime/services/network/scripts/samplefiles/forestbridge2.png'
-target = r'/Users/ariel/Documents/Developer/Web Projects/Lime/services/network/per/usr-2/forestbridge2.png'
+target = r'/Users/ariel/Documents/Developer/Web Projects/Lime/services/network/per/usr-3/forestbridge2.png'
 
 shutil.copyfile(original, target)
 
